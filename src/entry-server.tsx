306a66b25fdf176f53634e9c3143c40659224a39
Alex Yin <yinchihkai0909@gmail.com>
2024-02-19 13:19:03 +0800

    add react router dom

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 export function render() {
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
-      <div>hihi</div>
+      <App />
     </React.StrictMode>
   );
   return { html };
